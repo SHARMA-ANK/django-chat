5f82efba8e83da5d2e047df39acdadbce3d49bdd
SHARMA-ANK <ankitsharma_22248@aitpunr.edu.in>
2024-04-19 15:41:11 +0530

    WebSocket Created

--- djangochat/djangochat/settings.py
+++ djangochat/djangochat/settings.py
@@ -72,12 +72,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'djangochat.wsgi.application'
 ASGI_APPLICATION = 'djangochat.asgi.application'
 
-
+CHANNEL_LAYERS={
+    'default':{
+        'BACKEND':'channels.layers.InMemoryChannelLayer'
+    }
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
